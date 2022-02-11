c08d63e14c4c4b078c41879cd8e8feb7d8c4adec
Alexandre Garden <alexandre.garden@epitech.eu>
2022-02-11 15:15:33 +0100

    [ADD] change port 3000 to 80 for test domain

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-const port = 3000;
+const port = 80;
 
 const db = mysql.createPool({
     user: 'root',
