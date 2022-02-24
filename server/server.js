1c019dc61200fb16158385a216f3673c08b2b16a
Thomas RENON <thomas.renon@epitech.com>
2022-02-24 14:26:20 +0100

    [ADD] server with db

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const port = 3000;
 
 const db = mysql.createPool({
+    host: 'onearea.online:3006',
     user: 'root',
     password: 'password',
     database: 'area',
@@ -17,6 +18,60 @@ app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
+})
+
+const defaultServices = {
+    "weather": {
+        "is_active": false
+    },
+    "youtube": {
+        "is_active": false,
+        "token": ""
+    },
+}
+
+const defaultActions = { "actions": [] }
+
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const checkUsername = "select id from user where username=?";
+
+    db.query(checkUsername, [username], (err, result) => {
+        if (err) {
+            res.status(503).send(err.message);
+        }
+        else if (result.length >= 1) {
+            res.status(409).send(`Username ${username} already used`);
+        }
+        else {
+            const register = "insert into user (username, password, services, actions) values (?, ?, ?, ?)";
+
+            db.query(register, [username, password, JSON.stringify(defaultServices),
+                JSON.stringify(defaultActions)], (err, result) => {
+                    if (err)
+                        res.status(503).send(err.message);
+                    else
+                        res.send(result);
+                })
+        }
+    })
+})
+
+app.get("/test", (req, res) => {
+    const login = "select * from user";
+
+    db.query(login, (err, result) => {
+        if (err)
+            res.status(401).send("Test failed: " + err.message);
+        else {
+            res.send(result);
+        }
+    })
+})
+
+app.get("/", (req, res) => {
+    res.send("Server Instance V2");
 })
 
 app.post("/login", (req, res) => {
@@ -38,62 +93,28 @@ app.post("/login", (req, res) => {
     })
 })
 
-
-
-app.post("/register", (req, res) => {
+app.post("/service/active", (req, res) => {
+    const service_name = req.body.service_name;
+    const active_state = req.body.active_state;
     const username = req.body.username;
-    const password = req.body.password;
-    const checkUsername = "select id from user where username=?";
 
-    db.query(checkUsername, [username], (err, result) => {
+    const getServices = "select services from user where username=?";
+
+    db.query(getServices, [username], (err, result) => {
         if (err) {
             res.status(503).send(err.message);
         }
-        else if (result.length >= 1) {
-            res.status(409).send(`Username ${username} already used`);
-        }
         else {
-            const register = "insert into user (username, password) values (?, ?)";
+            var tmp = JSON.parse(result);
+            tmp[service_name].is_active = active_state;
+            const setServices = "update user set services=? where username=?";
 
-            db.query(register, [username, password], (err, result) => {
+            db.query(setServices, [tmp, username], (err, result) => {
                 if (err)
                     res.status(503).send(err.message);
                 else
                     res.send(result);
             })
         }
-    })
-})
-
-app.get("/test", (req, res) => {
-    const login = "select * from user";
-
-    db.query(login, (err, result) => {
-        if (err)
-            res.status(401).send("Test failed: " + err.message);
-        else {
-            res.send(result);
-        }
-    })
-})
-
-app.get("/", (req, res) => {
-    res.send("Server Instance V2");
-})
-
-
-app.post("/login", (req, res) => {
-    const token = req.body.token;
-    const login = "select id from user where username=? and password=?";
-
-    db.query(login, [username, password], (err, result) => {
-
-        console.log(result);
-        if (err)
-            res.status(503).send(err.message);
-        else if (result.length == 0)
-            res.status(401).send(`Username ${username} not found`);
-        else
-            res.send(result);
     })
 })
