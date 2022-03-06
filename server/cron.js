f46e51e464851fdfca44da76dd2598f5f7f26219
Thomas Renon <thomas.renon@epitech.eu>
2022-03-06 19:32:10 +0100

    cron

--- server/cron.js
+++ server/cron.js
@@ -1,6 +1,9 @@
 const cron = require('node-cron');
 const db = require('./database/mysql.js');
 const intra = require('./actions/intra.js');
+const weather = require('./actions/weather.js');
+const crypto = require('./actions/crypto.js');
+const covid = require('./actions/covid.js')
 const mailer = require('./service/mailService.js');
 
 function cronFunc() {
@@ -24,10 +27,28 @@ function cronFunc() {
                                 var _status = await intra.check_recent_absence(JSON.parse(user.services).intra.token);
                                 break;
                             case "check_credit":
+                                var _status = await intra.check_credit(JSON.parse(user.services).intra.token, action.number, action.symbol)
+                                break;
+                            case "check_gpa":
+                                var _status = await intra.check_gpa(JSON.parse(user.services).intra.token, action.number, action.symbol)
                                 break;
                             case "check_temperature":
+                                var _status = await weather.check_temperature(action.city, action.threshold);
+                                break;
+                            case "check_humidity":
+                                var _status = await weather.check_humidity(action.city, action.threshold);
                                 break;
                             case "check_crypto_value":
+                                var _status = await crypto.check_crypto_value(action.crypto, action.value, action.symbol);
+                                break;
+                            case "check_new_case":
+                                var _status = await covid.check_new_case(action.country, action.value, action, symbol);
+                                break;
+                            case "check_new_death":
+                                var _status = await covid.check_new_death(action.country, action.value, action, symbol);
+                                break;
+                            case "check_new_recovered":
+                                var _status = await covid.check_new_recovered(action.country, action.value, action, symbol);
                                 break;
                             case "check_new_comment":
                                 break;
