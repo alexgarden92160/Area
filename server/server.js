fc726c883b63012a5b8a4474aca854de9d038749
Alexandre Garden <alexandre.garden@epitech.eu>
2022-03-07 06:04:30 +0100

    [ADD] https

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const db = require('./database/mysql.js');
 const mailer = require("./service/mailService.js");;
 const DefaultActions = require('./default_actions.json')
 const DefaultServices = require('./default_services.json');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 const AboutJson = require('./about.json');
 const cron = require('./cron.js');
 const oauth = require('./oauth/google');
@@ -17,9 +20,20 @@ app.use(cors())
 cron();
 mailer.init();
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-})
+// app.listen(port, () => {
+//     console.log(`Server listening at http://localhost:${port}`);
+// })
+
+const sslServer = https.createServer(
+    {
+        key: fs.readFileSync(path.join(__dirname, 'cert', 'HSSL-61f1dbc334b67.key')),
+        ca: fs.readFileSync(path.join(__dirname, 'cert', 'AAACertificateServices.crt')),
+        cert: fs.readFileSync(path.join(__dirname, 'cert', 'onearea_online.crt'))
+    },
+    app
+);
+
+sslServer.listen(port, () => console.log('Secure server on port 3443'));
 
 app.get("/", (req, res) => {
     res.send("Server Instance V2");
