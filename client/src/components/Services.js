3f7df14f37ceb604ae3941be4a9f40b97c7bd82f
Ewen <ewen1.malenfant@epitech.eu>
2022-03-09 13:31:24 +0100

    [FIX] services

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -4,13 +4,14 @@ import axios from 'axios';
 function Services(props) {
   const [ac, setAc] = useState((sessionStorage.getItem(props.name) === 'true') === true ? "Disable" : "Enable")
   const [vari, setVari] = useState("")
+  const [ci, setCi] = useState("")
 
   const acdesac = async () => {
     if ((sessionStorage.getItem(props.name) === 'true') === false) {
       setAc("Disable")
       sessionStorage.setItem(props.name, true);
       await axios.post(
-        `https://onearea.online:3000/service/active/set`, {
+        `http://onearea.online:3000/service/active/set`, {
         service_name: props.name,
         active_state: true,
         id: sessionStorage.getItem("id")
@@ -20,7 +21,7 @@ function Services(props) {
       setAc("Enable")
       sessionStorage.setItem(props.name, false);
       await axios.post(
-        `https://onearea.online:3000/service/active/set`, {
+        `http://onearea.online:3000/service/active/set`, {
         service_name: props.name,
         active_state: false,
         id: sessionStorage.getItem("id")
@@ -32,13 +33,15 @@ function Services(props) {
     if (props.name === "intra") {
       return <input value={vari} onChange={(e) => setVari(e.target.value)} />
     }
-    if (props.name === "google")
-      setVari(sessionStorage.getItem("access-token"))
+    // else if (props.name === "google")
+    //   if (sessionStorage.getItem("access-token") !== undefined)
+    //     setCi(sessionStorage.getItem("access-token"))
     return
   }
 
   function send_token() {
-    axios.post(`https://onearea.online:3000/service/token/set`, {
+
+    axios.post(`http://onearea.online:3000/service/token/set`, {
       service_name: props.name,
       token: vari,
       id: sessionStorage.getItem("id")
